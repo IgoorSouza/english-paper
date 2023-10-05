0b0c594e2077c398bcfd9244c2d26b6f4ba95a99
Igor Souza <94133842+IgoorSouza@users.noreply.github.com>
2023-10-05 12:17:05 -0300

    first push

---
+++ script/script.js
@@ -0,0 +1,32 @@
+const close = document.querySelector("#close")
+const presentation = document.querySelector("#blur")
+const group = document.querySelector("#presentation")
+const translation = document.querySelector("#translation")
+const en = document.querySelector("#en")
+const pt = document.querySelector("#pt")
+const enContent = document.querySelector("#english")
+const ptContent = document.querySelector("#portuguese")
+
+close.addEventListener('click', () => {
+  group.setAttribute("id", "closePresentation")
+  presentation.setAttribute("id", "closePresentation")
+  translation.style.display = 'flex'
+})
+
+pt.addEventListener('click', () => {
+  enContent.classList.add("changeLang")
+  ptContent.classList.remove("changeLang")
+  pt.style.backgroundColor = 'green'
+  pt.style.color = 'white'
+  en.style.backgroundColor = 'white'
+  en.style.color = 'black'
+})
+
+en.addEventListener('click', () => {
+  ptContent.classList.add("changeLang")
+  enContent.classList.remove("changeLang")
+  en.style.backgroundColor = 'blue'
+  en.style.color = 'white'
+  pt.style.backgroundColor = 'white'
+  pt.style.color = 'black'
+})
